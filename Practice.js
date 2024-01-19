af464c53be83efe8fb75e7b315c3222b6a2e1b10
trivickram <143883198+trivickram@users.noreply.github.com>
2024-01-19 23:21:53 +0530

    Update Practice.js

--- Practice.js
+++ Practice.js
@@ -1,11 +1,1 @@
-var clothingItem = {
-    price: 50,
-    color: 'beige',
-    material: 'cotton',
-    season: 'autumn'
-}
-
-for( const key of Object.keys(clothingItem) ) {
-    console.log(key, ":", clothingItem[key])
-}
-console.log(`${1 + 1 + "1" + 1 + 1} stars!`) 
+console.log("hello world")
