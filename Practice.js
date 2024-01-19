d422c43801b297d5823e8a6751f3e208f554a049
trivickram <baratamamrutha04@gmail.com>
2024-01-19 23:18:43 +0530

    .

---
+++ Practice.js
@@ -0,0 +1,11 @@
+var clothingItem = {
+    price: 50,
+    color: 'beige',
+    material: 'cotton',
+    season: 'autumn'
+}
+
+for( const key of Object.keys(clothingItem) ) {
+    console.log(key, ":", clothingItem[key])
+}
+console.log(`${1 + 1 + "1" + 1 + 1} stars!`) 
